c30375552650a477dd2f23cdf0ff657f2168b7ae
Eli Lieberman <35276371+elilieberman@users.noreply.github.com>
2020-04-26 22:17:53 +0300

    Add files via upload

--- covid19d.py
+++ covid19d.py
@@ -4,9 +4,16 @@ Created on Mon Mar 16 16:06:13 2020
 
 @author: runner
 """
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 22 15:24:43 2020
+
+@author: runner
+"""
 import numpy as np
 import pandas as pd
 import os
+import sys # for utilities that check memory issues
 os.environ['PROJ_LIB'] = r'C:\EliPersonal\Python\Datasets\ieee' # fixes cant find "epsg" and PROJ_LIB for BASEMAP
 pd.set_option('display.max_columns', None)
 pd.option_context('display.max_colwidth', None)
@@ -15,6 +22,7 @@ pd.set_option('display.max_colwidth', None) #dont truncate column value display
 pd.set_option('display.max_columns', None) # .head will show all
 pd.set_option('display.max_rows', None) # .head will show all
 #%% https://worldpopulationreview.com/#liveWorldPop population stats
+# cov = pd.read_csv(r'C:\EliPersonal\Python\Datasets\Doit\covid.csv', parse_dates = [2], infer_datetime_format = True, index_col = [1,2])
 # Import tables
 c_usecols = ["Province/State", "Country/Region", "Lat", "Long", "Date","Value", "ISO 3166-1 Alpha 3-Codes"]
 confirmed = pd.read_csv('https://data.humdata.org/hxlproxy/data/download/time_series_covid19_confirmed_global_narrow.csv?dest=data_edit&filter01=merge&merge-url01=https%3A%2F%2Fdocs.google.com%2Fspreadsheets%2Fd%2Fe%2F2PACX-1vTglKQRXpkKSErDiWG6ycqEth32MY0reMuVGhaslImLjfuLU0EUgyyu2e-3vKDArjqGX7dXEBV8FJ4f%2Fpub%3Fgid%3D1326629740%26single%3Dtrue%26output%3Dcsv&merge-keys01=%23country%2Bname&merge-tags01=%23country%2Bcode%2C%23region%2Bmain%2Bcode%2C%23region%2Bsub%2Bcode%2C%23region%2Bintermediate%2Bcode&filter02=merge&merge-url02=https%3A%2F%2Fdocs.google.com%2Fspreadsheets%2Fd%2Fe%2F2PACX-1vTglKQRXpkKSErDiWG6ycqEth32MY0reMuVGhaslImLjfuLU0EUgyyu2e-3vKDArjqGX7dXEBV8FJ4f%2Fpub%3Fgid%3D398158223%26single%3Dtrue%26output%3Dcsv&merge-keys02=%23adm1%2Bname&merge-tags02=%23country%2Bcode%2C%23region%2Bmain%2Bcode%2C%23region%2Bsub%2Bcode%2C%23region%2Bintermediate%2Bcode&merge-replace02=on&merge-overwrite02=on&filter03=explode&explode-header-att03=date&explode-value-att03=value&filter04=rename&rename-oldtag04=%23affected%2Bdate&rename-newtag04=%23date&rename-header04=Date&filter05=rename&rename-oldtag05=%23affected%2Bvalue&rename-newtag05=%23affected%2Binfected%2Bvalue%2Bnum&rename-header05=Value&filter06=clean&clean-date-tags06=%23date&filter07=sort&sort-tags07=%23date&sort-reverse07=on&filter08=sort&sort-tags08=%23country%2Bname%2C%23adm1%2Bname&tagger-match-all=on&tagger-default-tag=%23affected%2Blabel&tagger-01-header=province%2Fstate&tagger-01-tag=%23adm1%2Bname&tagger-02-header=country%2Fregion&tagger-02-tag=%23country%2Bname&tagger-03-header=lat&tagger-03-tag=%23geo%2Blat&tagger-04-header=long&tagger-04-tag=%23geo%2Blon&header-row=1&url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_covid19_confirmed_global.csv', parse_dates = [2], infer_datetime_format = True, usecols = c_usecols,skiprows=[1] ) #first row has duplicate headers that result in mistyped columns
@@ -59,6 +67,7 @@ cov['log_conf'] = np.log(cov['confirmed'])
 #%% Within a Country,SUM all State values by Date
 np.seterr(divide = 'ignore') #turn off warning for dividing by missing values
 cov2 = cov.groupby(['Country/Region','Last_Update'], as_index = False).agg({'confirmed':'sum','deaths':'sum','recovered':'sum', '2018':'max', 'Lat':'max', 'Long':'max'})
+#cov22 = cov.groupby(['Country_Region','Last_Update'], as_index = False).agg({'Confirmed':'sum','Deaths':'sum','Recovered':'sum', 'pop2020':'max'})
 cov2.rename(columns={'2018': 'pop'}, inplace=True)
 cov2.sort_values(['Country/Region','Last_Update'], ascending= [True,True], inplace=True)
 cov2.set_index(['Country/Region', 'Last_Update'], inplace = True)
@@ -74,6 +83,7 @@ cov2['recv_rate'] = cov2['cum_Recv']/cov2['cum_Conf']
 cov2['pct_conf'] = cov2['cum_Conf']/cov2['pop']
 cov2['pct_recv'] = cov2['cum_Recv']/cov2['pop']
 cov2['pct_conf'] = cov2['cum_Conf']/cov2['pop']
+#cov2[['ConfChg','RecvChg']] = cov2.sort_values(['Country_Region','Last_Update'], ascending= [True,True]).groupby(level=0)[['cum_Conf','cum_Recv' ]].pct_change()
 cov2[['ConfChg','RecvChg']] = cov2.groupby(level=0)[['cum_Conf','cum_Recv' ]].pct_change()
 world_pop = cov2['pop'].sum() # missing data from country name mismatches at merge
 conf_rt = cov2['Rolling_Rank'].mean()
@@ -84,7 +94,7 @@ cov2.tail(14)
 cov_all =  cov2.reset_index(level=0, drop=True)
 cov_all.sort_values('Last_Update', ascending= True, inplace=True)
 cov_all['worldwide_cum_confirmed'] = cov_all['confirmed'].cumsum()
-#print ('Total Confirmed Covid Cases Worldwide: ',cov_all['confirmed'].cumsum().max())
+print ('Total Confirmed Covid Cases Worldwide: ',cov_all['confirmed'].cumsum().max())
 #%% Save Master data to csv for PowerBI, and RANKINGS
 agg_data = cov2.reset_index()    
 agg_data.sort_values(['Country/Region','Last_Update'], ascending= [True,True], inplace=True)
@@ -94,40 +104,38 @@ Rankings = agg_data.groupby('Country/Region')['Rolling_Rank'].last()
 Rankings = Rankings.reset_index()
 Rankings.dropna().sort_values(by = ['Rolling_Rank'], ascending = True)
 agg_data.to_csv(r'C:\EliPersonal\Python\Datasets\Doit\agg_data.csv', encoding = 'utf-8', index = False) # save data set with features to csv
+#agg_data.query('(Country_Region == "Israel") and ()').head() #query doesn't work with slashes in column names
+#test Azure created csv
+# zz = pd.read_csv('https://covid19worldwideactivity-elilieberman.notebooks.azure.com/j/lab/tree/agg_data.csv', lineterminator='\n', encoding = 'utf-8', engine='python', sep=',', error_bad_lines=False)
 #%% Plot Country Trend
 import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 country_selected = 'Israel'  #used for graphs and ARIMA
-plot_title_a = 'COVID Trend using Rolling Mean, ' + country_selected 
-
+plot_title_a = 'COVID Trend using Rolling Mean'  + country_selected
+      
 plot_data = cov2.reset_index()    
 plot_data.sort_values(['Country/Region','Last_Update'], ascending= [True,True], inplace=True)
 plot_data.head()
 plot_data = plot_data.loc[:,['Country/Region','Last_Update','log_confirmed', '5_rollmean_confirmed','pct_conf', 'pct_recv']]
 plots = plot_data[plot_data['Country/Region'] == country_selected]
-
-plt.close()
-sns.set(style="white", rc={"lines.linewidth": 3})
-fig, ax1 = plt.subplots(figsize=(8,4))
 sns.lineplot(x='Last_Update', 
              y='5_rollmean_confirmed',
              data = plots,
-             color='r',
-             marker='>',
+             color='g',
+             marker='D',
              linewidth = 1,
              linestyle = '-',
-             ax=ax1)
+             ax=ax2)
 ax1.tick_params('both', labelrotation=45)
 ax1.xaxis.grid(True)
 ax1.yaxis.grid(True)
 ax1.set_xlabel('')
 plt.title(plot_title_a)
 plt.show()
-plt.savefig(r'C:\EliPersonal\Python\Datasets\Doit\COVID_Trend_Italy.png')
+plt.savefig(r'C:\EliPersonal\Python\Datasets\Doit\COVID_Trend_Israel.png')
 sns.set()
 #%% Plot data as a Percent of POPULATION
-#plt.close()
 plot_title_b = 'COVID Confirmed v. Recovering, Population Data ' + country_selected +'\n(Confirmed is red line, Recovered is green line)'
 sns.set(style="white", rc={"lines.linewidth": 3})
 fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1,figsize=(6,3), sharex = True)
@@ -155,6 +163,7 @@ ax2.yaxis.grid(True)
 ax1.set_xlabel('')
 ax1.set_title(plot_title_b, pad =20)
 plt.show()
+plt.savefig(r'C:\EliPersonal\Python\Datasets\Doit\COVID_Israel_Population_Data.png')
 sns.set()
 #%% Read Israel SYMPTOM data using JSON api, convert RESULTS dictionary to dataframe
 import pandas as pd
@@ -169,7 +178,7 @@ isr_sym['over60'] = isr_sym['age_60_and_above'].eq('Yes').astype(int) #convert s
 isr_sym[['age_60_and_above','over60']].tail(20)
 isr_sym.info()
 
-#%%  Linear Regression Model
+#%%  Linear Regression Model, Israel Test Data
 labels = ['cough', 'fever', 'sore_throat','shortness_of_breath', 'head_ache', 'male', 'over60'] #'test_date', 'test_indication',
 X = isr_sym[labels]
 X = X.apply(pd.to_numeric, errors='coerce')
@@ -196,8 +205,9 @@ print(isr_sym_ratio.sort_values(by = 'weight', ascending = False))
 chart_info = 'Top Symptoms, COVID Coefficient Weights, Source: gov.co.il\n' + 'Number Test Conducted ' + str(max(isr_sym._id))+ ',' + ' Data Updated ' + str(max(isr_sym.test_date))
 isr_sym_ratio.sort_values(by = 'weight', ascending = False).head(5).plot(kind='bar', x = 'symptom', y = 'weight', title = chart_info, color=['r', 'g', 'b', 'y', 'm', 'k', 'c'], legend = None)
 plt.ylabel('Weight/Importance', fontsize=10)
-plt.xticks(rotation=45, ha="right")   #for x-axis legend labels to fit properly
-plt.tight_layout() #for x-axis legend labels to fit properly
+plt.xticks(rotation=45, ha="right")   #fpr x-axis legend labels to fit properly
+plt.tight_layout() #fpr x-axis legend labels to fit properly
+plt.savefig(r'C:\EliPersonal\Python\Datasets\Doit\COVID_Israel_Testing_Data.png')
 #%% ARIMA TIME SERIES ANALYSIS (TSA) Forecasting
 from statsmodels.tsa.arima_model import ARIMA
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
@@ -271,4 +281,5 @@ ax1.set_xlabel('')
 ax1.set_ylabel('Confirmed Cases and Predicted')
 plt.title(plot_title_arima_pred)
 plt.show()
+plt.savefig(r'C:\EliPersonal\Python\Datasets\Doit\COVID_Israel_Forecast_Data.png')
 sns.set()
